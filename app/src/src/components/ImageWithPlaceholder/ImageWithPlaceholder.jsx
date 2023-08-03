61c106494979c532a7b557088a3b98015765006d
Александр Лебедев <64793044+Sanch0pansa@users.noreply.github.com>
2023-08-03 15:29:42 +0300

    Created Image with placeholder component

---
+++ app/src/src/components/ImageWithPlaceholder/ImageWithPlaceholder.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import Image from "next/image";
+
+const imageLoader = ({ src }) => {
+    return `${src}`
+}
+
+const ImageWithPlaceholder = ({
+    src,
+    srcLow,
+    ...imageProps
+}) => {
+    const [loading, setLoading] = useState(true);
+    const [showFull, setShowFull] = useState(false);
+
+
+    return (
+        <>
+            {
+                loading ? (
+                    <Image
+                        src={srcLow}
+                        loader={imageLoader}
+                        {...imageProps}
+                    />
+                ) : ""
+            }
+            <Image
+                src={src}
+                style={{visibility: !showFull ? "hidden" : "visible"}}
+                loader={imageLoader}
+                onLoad={() => {
+                    setShowFull(true);
+                    setTimeout(() => {
+                        setLoading(false);
+                    }, 100);
+                    imgOnLoad();
+                }}
+                {...imageProps}
+            />
+        </>
+    );
+};
+
+export default ImageWithPlaceholder;
